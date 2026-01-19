344e9b69152b684fd221515fe80186e4d85260c3
yeri kwon <yeri8791@gmail.com>
2026-01-19 14:13:14 +0900

    ch3 : img (ver2)

---
+++ chapter3/step_2_1.py
@@ -0,0 +1,13 @@
+from PIL import Image
+from step_1_1 import IMG_DIR
+from PIL import ImageOps
+
+img = Image.open(IMG_DIR/"img_001.png")
+#print(f"{img.size=},{img.format=}, {img.mode=}")
+
+SIZE = (500,500)
+img_resize = img.resize(SIZE)
+# print(f"{img_resize.size=}")
+
+img_cont = ImageOps.contain(img,SIZE)
+print(f"{img_cont.size=}")
